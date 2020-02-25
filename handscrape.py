e24f5f02b8ac39c5d76372389981f4c10d30c7ac
rainforestpuppy <61443129+mellowpuppy@users.noreply.github.com>
2020-02-25 15:10:47 -0500

    Create handscrape.py

---
+++ handscrape.py
@@ -0,0 +1,13 @@
+#break program into multiple files
+import .models
+import .views
+
+#run searches
+
+#output errors
+
+#if okay, scrape data and sort
+
+#if okay, execute views (sort and post to wp)
+
+#output any errors
