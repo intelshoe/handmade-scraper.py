0cd7b7081fe3e48aa767882e4639a641870a1105
rainforestpuppy <61443129+mellowpuppy@users.noreply.github.com>
2020-03-21 19:54:09 -0400

    Update search.py

--- search.py
+++ search.py
@@ -2,10 +2,13 @@
 # import needed modules
             
 #define google search function 
+#accepts 
 #if user chose google then
     #if user chose terms to use with every search, then
             
     #elif user wants to add terms to keyword list, then
+        #add terms to search term list
+            
 #else
 #print("Invalid search options")            
 
