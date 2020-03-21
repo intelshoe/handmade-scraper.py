899b435b0d9569bee68ff617332349217d3cfd94
rainforestpuppy <61443129+mellowpuppy@users.noreply.github.com>
2020-03-21 19:37:33 -0400

    Update search.py

--- search.py
+++ search.py
@@ -6,10 +6,20 @@ import urllib.request
 #function to perform search
 def search(query):
    URL = "https://www.google.com/search?q=StackOverflow"
-   headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+   headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 
+            (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
    resp = requests.get(URL, headers=headers).text 
    soup = BeautifulSoup(resp, 'html.parser')
    #exampled of what is possible to find
    for link in soup.findAll('a', href=True): 
        if 'facebook.com' in link.get('href'):
            print link.get('href')
+            
+ #define search function
+ 
+#if user chose google then
+    #if user chose terms to use with every search, then
+            
+    #elif user wants to add terms to keyword list, then
+#else
+print("Invalid search options")            
