7c67fc4f40c139797ea824d8058fd7c677072306
rainforestpuppy <61443129+mellowpuppy@users.noreply.github.com>
2020-03-21 16:10:31 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,12 +3,26 @@
 import .search
 import .parse
 
-#run searches
+#user inputs search engine preference
+#big ones: google, bing, yahoo
+#or ecommerce site search: etsy, amazon, craigslist, facebook markets, small forums, private sites
+#and even sub-sites of the big search engines related to shopping
 
-#output errors
+#user may choose several engines, which will randomize searches through them
 
-#if okay, scrape data and sort
+#user chooses "Dorks" for optimizing search
+#options could be default, random, or custom
+#custom allows dorks for beginning, middle, and end of search string
 
-#if okay, execute views (sort and post to wp)
+#user chooses keyword list
+#default or custom
 
-#output any errors
+#user chooses amount of keywords to use at one time
+#1 term only, 2 terms, 3 terms, 4 terms, or random
+
+#user inputs additional terms, such as dates, up to 4 - optional
+#user has option to add to regular term list or use in every search
+
+#finally user inputs amount of potentials to find in total
+#default = 50
+
