ad2aaa6afa4385876f6cd67d1669c50f5114a847
rainforestpuppy <61443129+mellowpuppy@users.noreply.github.com>
2020-03-21 15:31:05 -0400

    Update and rename googlesearch.py to search.py

--- googlesearch.py
+++
@@ -1,8 +0,0 @@
-# This script will search google using our web dorks file and search term file
-# import needed files
-import requests
-import urllib.request
-
-#function to perform search
-def search(query):
-  url = "http://www.google.com/search?q="+query
